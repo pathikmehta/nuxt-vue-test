bc028246732897d31b1557e6231f368e510227e3
Pathik Mehta <pathikmehta11@gmail.com>
2020-04-08 18:45:28 +0530

    Intergrated search joke api

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes v-on:search-text="searchText" />
     <Joke
       v-for="joke in jokes"
       :key="joke.id"
@@ -12,10 +13,12 @@
 <script>
 import axios from 'axios'
 import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
 
 export default {
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data () {
     return {
@@ -34,6 +37,21 @@ export default {
       this.jokes = res.data.results
     } catch (err) {
       console.log(err)
+    }
+  },
+  methods: {
+    async searchText (text) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config)
+        this.jokes = res.data.results
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
   head () {
