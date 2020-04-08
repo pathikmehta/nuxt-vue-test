d76faed8206717a2523ec510c43692a11163c3f8
Pathik Mehta <pathikmehta11@gmail.com>
2020-04-08 10:37:12 +0530

    Added new page (component) for about page with its metadata and head details. Adding this in pagees automatically redered that component on (about) route.

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>About Jokes</h1>
+    <p>This is an app that displays jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'About The App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
